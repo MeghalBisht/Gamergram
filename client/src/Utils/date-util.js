b1df3106462fd6fd7aa0ae6c6a49db9de64defa0
Matteo A <augermtt@gmail.com>
2020-10-12 22:26:34 +0200

    feat(post): add date on post

---
+++ client/src/Utils/date-util.js
@@ -0,0 +1,28 @@
+export const relativeDate = (timestamp) => {
+  const NB_DAYS_YEAR = 365; // nb days on a year
+  const NB_HOURS_DAY = 24; // nb hours on a day
+  const NB_MINUTES_HOUR = 60; // nb minutes on a hour
+  const diffMs = Date.now() - new Date(timestamp).getTime();
+
+  const diffMinutes = Math.floor(diffMs / 60000);
+  const diffHours = Math.floor(diffMinutes / NB_MINUTES_HOUR);
+  const diffDays = Math.floor(diffHours / NB_HOURS_DAY);
+  const diffYears = Math.floor(diffDays / NB_DAYS_YEAR);
+
+  if (diffMinutes <= 2) return "Posted a few minutes ago";
+
+  if (diffMinutes < NB_MINUTES_HOUR) return `Posted ${diffMinutes} minutes ago`;
+
+  if (diffHours < NB_HOURS_DAY) {
+    const hourLabel = diffHours === 1 ? "hour" : "hours";
+    return `Posted ${diffHours} ${hourLabel} ago`;
+  }
+
+  if (diffDays < NB_DAYS_YEAR) {
+    const dayLabel = diffDays === 1 ? "day" : "days";
+    return `Posted ${diffDays} ${dayLabel} ago`;
+  }
+
+  const yearLabel = diffYears === 1 ? "year" : "years";
+  return `Posted ${diffYears} ${yearLabel} ago`;
+};

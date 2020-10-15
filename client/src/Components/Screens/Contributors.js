b843549d42ec4156bcfdfb20bca9c21750ee3a5b
EnechukwuChibuike <enechukwuchibuike@gmail.com>
2020-10-15 09:14:55 -0400

    Added Contributors page

---
+++ client/src/Components/Screens/Contributors.js
@@ -0,0 +1,160 @@
+import React from "react";
+import Pic from "../../Images/pic.png";
+import "../../Styles/Contributors.css";
+import { Link } from "react-router-dom";
+
+export default function Contributors() {
+   return (
+      <>
+         <div className="contributors">
+            <p>The Contributors to this website</p>
+         </div>
+         <div className="Main">
+            <div className="Sub">
+               <img className="Pic" src={Pic} alt="picture" />
+               <p>De Saint Enechukwu</p>
+               <button type="submit" className="btn waves-effect waves-light">
+                  <a
+                     style={{ color: "#d6efc7" }}
+                     href="https://github.com/EnechukwuChibuike"
+                  >
+                     Github account
+                  </a>
+               </button>
+
+               <button type="submit" className="btn waves-effect waves-light">
+                  <Link style={{ color: "#d6efc7" }} to="/create">
+                     Contribution
+                  </Link>
+               </button>
+            </div>
+            <div className="Sub">
+               <img className="Pic" src={Pic} alt="picture" />
+               <p>De Saint Enechukwu</p>
+               <button type="submit" className="btn waves-effect waves-light">
+                  <a
+                     style={{ color: "#d6efc7" }}
+                     href="https://github.com/EnechukwuChibuike"
+                  >
+                     Github account
+                  </a>
+               </button>
+
+               <button type="submit" className="btn waves-effect waves-light">
+                  <Link style={{ color: "#d6efc7" }} to="/create">
+                     Contribution
+                  </Link>
+               </button>
+            </div>
+            <div className="Sub">
+               <img className="Pic" src={Pic} alt="picture" />
+               <p>De Saint Enechukwu</p>
+               <button type="submit" className="btn waves-effect waves-light">
+                  <a
+                     style={{ color: "#d6efc7" }}
+                     href="https://github.com/EnechukwuChibuike"
+                  >
+                     Github account
+                  </a>
+               </button>
+
+               <button type="submit" className="btn waves-effect waves-light">
+                  <Link style={{ color: "#d6efc7" }} to="/create">
+                     Contribution
+                  </Link>
+               </button>
+            </div>
+            <div className="Sub">
+               <img className="Pic" src={Pic} alt="picture" />
+               <p>De Saint Enechukwu</p>
+               <button type="submit" className="btn waves-effect waves-light">
+                  <a
+                     style={{ color: "#d6efc7" }}
+                     href="https://github.com/EnechukwuChibuike"
+                  >
+                     Github account
+                  </a>
+               </button>
+
+               <button type="submit" className="btn waves-effect waves-light">
+                  <Link style={{ color: "#d6efc7" }} to="/create">
+                     Contribution
+                  </Link>
+               </button>
+            </div>
+            <div className="Sub">
+               <img className="Pic" src={Pic} alt="picture" />
+               <p>De Saint Enechukwu</p>
+               <button type="submit" className="btn waves-effect waves-light">
+                  <a
+                     style={{ color: "#d6efc7" }}
+                     href="https://github.com/EnechukwuChibuike"
+                  >
+                     Github account
+                  </a>
+               </button>
+
+               <button type="submit" className="btn waves-effect waves-light">
+                  <Link style={{ color: "#d6efc7" }} to="/create">
+                     Contribution
+                  </Link>
+               </button>
+            </div>
+            <div className="Sub">
+               <img className="Pic" src={Pic} alt="picture" />
+               <p>De Saint Enechukwu</p>
+               <button type="submit" className="btn waves-effect waves-light">
+                  <a
+                     style={{ color: "#d6efc7" }}
+                     href="https://github.com/EnechukwuChibuike"
+                  >
+                     Github account
+                  </a>
+               </button>
+
+               <button type="submit" className="btn waves-effect waves-light">
+                  <Link style={{ color: "#d6efc7" }} to="/create">
+                     Contribution
+                  </Link>
+               </button>
+            </div>
+            <div className="Sub">
+               <img className="Pic" src={Pic} alt="picture" />
+               <p>De Saint Enechukwu</p>
+               <button type="submit" className="btn waves-effect waves-light">
+                  <a
+                     style={{ color: "#d6efc7" }}
+                     href="https://github.com/EnechukwuChibuike"
+                  >
+                     Github account
+                  </a>
+               </button>
+
+               <button type="submit" className="btn waves-effect waves-light">
+                  <Link style={{ color: "#d6efc7" }} to="/create">
+                     Contribution
+                  </Link>
+               </button>
+            </div>
+            <div className="Sub">
+               <img className="Pic" src={Pic} alt="picture" />
+               <p>De Saint Enechukwu</p>
+               <button type="submit" className="btn waves-effect waves-light">
+                  <a
+                     style={{ color: "#d6efc7" }}
+                     href="https://github.com/EnechukwuChibuike"
+                  >
+                     Github account
+                  </a>
+               </button>
+
+               <button type="submit" className="btn waves-effect waves-light">
+                  <Link style={{ color: "#d6efc7" }} to="/create">
+                     Contribution
+                  </Link>
+               </button>
+            </div>
+         </div>
+      </>
+   );
+}
--- client/src/Components/Screens/Footer.js
+++ client/src/Components/Screens/Footer.js
@@ -1,30 +1,60 @@
-import React from 'react'
-import "../../Styles/Footer.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../Styles/Footer.css";
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <div className="container text-center">
-                <img src="../../gamergram.ico" className="d-block mb-4 mx-auto" width="100px " />    
-            
-            <p className="text-secondary col md 9 mx-auto">A web application to publish your thoughts, connect with friends, share pictures, comment, like and much more!</p>
-                <p className="text-secondary col md 9 mx-auto">Developed by Meghal Bisht using the MERN stack.</p>
+   return (
+      <footer className="footer">
+         <div className="container text-center">
+            <img
+               src="../../gamergram.ico"
+               className="d-block mb-4 mx-auto"
+               width="100px "
+            />
+
+            <p className="text-secondary col md 9 mx-auto">
+               A web application to publish your thoughts, connect with friends,
+               share pictures, comment, like and much more!
+            </p>
+            <p className="text-secondary col md 9 mx-auto">
+               Developed by Meghal Bisht using the MERN stack.
+            </p>
+            <Link className="Link" to="/contributors">
+               Contributors
+            </Link>
             <hr className="my-5" />
             <div className="mb-3">
-                <a rel="noopener noreferrer" target="_blank" className="ctx__icons"  href="https://www.linkedin.com/in/meghal-bisht-777451177/">
-                    <i className="fab fa-linkedin"></i>
-                </a>
-                <a rel="noopener noreferrer" target="_blank"  className="ctx__icons" href="https://github.com/MeghalBisht">
-                    <i className="fab fa-github"></i>
-                </a>
-                <a rel="noopener noreferrer" target="_blank" className="ctx__icons"  href="https://resume-meghal.herokuapp.com/">
-                    <i className="fas fa-code"></i>
-                </a>
-                </div>
-                <p className="text-secondary">Copyright 2020. All rights reserved!</p>
+               <a
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="ctx__icons"
+                  href="https://www.linkedin.com/in/meghal-bisht-777451177/"
+               >
+                  <i className="fab fa-linkedin"></i>
+               </a>
+               <a
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="ctx__icons"
+                  href="https://github.com/MeghalBisht"
+               >
+                  <i className="fab fa-github"></i>
+               </a>
+               <a
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="ctx__icons"
+                  href="https://resume-meghal.herokuapp.com/"
+               >
+                  <i className="fas fa-code"></i>
+               </a>
             </div>
-        </footer>
-    )
+            <p className="text-secondary">
+               Copyright 2020. All rights reserved!
+            </p>
+         </div>
+      </footer>
+   );
 }
 
-export default Footer
+export default Footer;
